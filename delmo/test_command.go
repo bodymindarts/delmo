91bcaaed4730a4a5aa8090f139bc2d968fc043d3
Justin Carter <jcarter@starkandwayne.com>
2016-05-30 00:40:30 -0700

    Rename dockerCompose -> system

--- delmo/test_command.go
+++ delmo/test_command.go
@@ -39,19 +39,19 @@ func (t *TestCommand) Run(args []string) int {
 		t.Ui.Error(fmt.Sprintf("%v", err))
 		return 1
 	}
-	dockerCompose, err := NewDockerCompose(suite.ComposeFile, "delmo")
+	system, err := NewDockerCompose(suite.ComposeFile, "delmo")
 	if err != nil {
 		t.Ui.Error(fmt.Sprintf("%s", err))
 		return 1
 	}
 	t.Ui.Info(fmt.Sprintf("Starting System %s", suite.System.Name))
-	dockerCompose.Start()
+	system.Start()
 	t.Ui.Info("Waiting 2 seconds")
 	time.Sleep(2 * time.Second)
 	t.Ui.Info("Stopping System")
-	dockerCompose.Stop()
+	system.Stop()
 	t.Ui.Info("Reading output")
-	out, err := ioutil.ReadAll(dockerCompose.Output())
+	out, err := ioutil.ReadAll(system.Output())
 	t.Ui.Info(string(out))
 	return 0
 
