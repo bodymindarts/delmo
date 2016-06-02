d0dbddb43102e2ddb0a7d1f1b21e6e7e8533f1e3
Justin Carter <jcarter@starkandwayne.com>
2016-06-02 15:31:04 +0200

    Add integration test

---
+++ ci/tasks/integration-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export GOPATH=$PWD/delmo:$GOPATH
+cd delmo/src/github.com/bodymindarts/delmo
+
+echo "Building binary"
+go build -o bin/delmo
+
+echo "Downloading machine info"
+aws --region ${AWS_REGION} s3 cp s3://${AWS_BUCKET}/${machine_name}.zip ${machine_name}.zip
+
+echo "Importing ${machine_name}"
+machine-import ${machine_name}
+
+echo "Evaluating environment"
+eval $(docker-machine env ${machine_name})
+
+bin/delmo -f example/webapp/delmo.yml
