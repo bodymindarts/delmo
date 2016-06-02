5b644a3e70e17aef2809c838f8837b567e887b00
Justin Carter <jcarter@starkandwayne.com>
2016-06-02 15:35:02 +0200

    Fix file ending

--- ci/tasks/integration-test.sh
+++ ci/tasks/integration-test.sh
@@ -10,7 +10,7 @@ echo "Downloading machine info"
 aws --region ${AWS_REGION} s3 cp s3://${AWS_BUCKET}/${machine_name}.zip ${machine_name}.zip
 
 echo "Importing ${machine_name}"
-machine-import ${machine_name}
+machine-import ${machine_name}.zip
 
 echo "Evaluating environment"
 eval $(docker-machine env ${machine_name})
