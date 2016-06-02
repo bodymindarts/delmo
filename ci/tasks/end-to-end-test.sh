33af4e177d4c4c5378669de80021fecef8d161f2
Justin Carter <jcarter@starkandwayne.com>
2016-06-02 16:50:28 +0200

    Fix permissions in end-to-end test

--- ci/tasks/end-to-end-test.sh
+++ ci/tasks/end-to-end-test.sh
@@ -11,6 +11,8 @@ aws --region ${AWS_REGION} s3 cp s3://${AWS_BUCKET}/${machine_name}.zip ./
 
 echo "Importing ${machine_name}"
 machine-import ${machine_name}.zip
+# The permission isn't set properly on import
+chmod 0600 /root/.docker/machine/machines/${machine_name}/id_rsa
 
 echo "Setting up environment"
 eval $(docker-machine env "${machine_name}" --shell bash)
