b692bdec75bc5acd955aaee259fdf402a6614104
Justin Carter <jcarter@starkandwayne.com>
2016-06-02 15:39:18 +0200

    Fix s3 download path

--- ci/tasks/integration-test.sh
+++ ci/tasks/integration-test.sh
@@ -7,7 +7,7 @@ echo "Building binary"
 go build -o bin/delmo
 
 echo "Downloading machine info"
-aws --region ${AWS_REGION} s3 cp s3://${AWS_BUCKET}/${machine_name}.zip ${machine_name}.zip
+aws --region ${AWS_REGION} s3 cp s3://${AWS_BUCKET}/${machine_name}.zip ./
 
 echo "Importing ${machine_name}"
 machine-import ${machine_name}.zip
