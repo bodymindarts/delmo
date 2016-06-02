09713fa2738db6f73e85a8413aeb2f2b29e93e3d
Justin Carter <jcarter@starkandwayne.com>
2016-06-02 15:07:15 +0200

    Upload files to docker-machine host

--- delmo/config.go
+++ delmo/config.go
@@ -20,6 +20,7 @@ type SystemConfig struct {
 	File             string `yaml:"file"`
 	CompleteFilePath string
 	Services         map[string]ServiceConfig
+	MachineName      string `yaml:"machine-name"`
 }
 
 type TaskConfig struct {
--- delmo/test_command.go
+++ delmo/test_command.go
@@ -3,6 +3,9 @@ package delmo
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/mitchellh/cli"
@@ -38,6 +41,13 @@ func (t *TestCommand) Run(args []string) int {
 		t.Ui.Error(fmt.Sprintf("Error reading configuration\n%s", err))
 		return 2
 	}
+
+	_, err = t.prepareDockerHost(path, config.System)
+	if err != nil {
+		t.Ui.Error(fmt.Sprintf("Cloud not setup docker-machine\n%s", err))
+		return 2
+	}
+
 	suite, err := NewSuite(config)
 	if err != nil {
 		t.Ui.Error(fmt.Sprintf("Could not initialize suite %s"))
@@ -50,4 +60,57 @@ func (t *TestCommand) Run(args []string) int {
 
 func (t *TestCommand) Synopsis() string {
 	return "Run some tests"
+}
+
+func (t *TestCommand) prepareDockerHost(path string, system SystemConfig) (string, error) {
+	rawCmd, err := exec.LookPath("docker-machine")
+	if err != nil {
+		return "", err
+	}
+	hostDir := fmt.Sprintf(".delmo/%s", system.Name)
+
+	args := []string{
+		"ssh",
+		system.MachineName,
+		"rm",
+		"-rf",
+		hostDir,
+	}
+	cmd := exec.Command(rawCmd, args...)
+	err = cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("Could not delete dir %s\n%s", hostDir, err)
+	}
+
+	args = []string{
+		"ssh",
+		system.MachineName,
+		"mkdir",
+		"-p",
+		hostDir,
+	}
+	cmd = exec.Command(rawCmd, args...)
+	err = cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("Could not create dir %s\n%s", hostDir, err)
+	}
+
+	dir := filepath.Dir(path)
+	files, err := ioutil.ReadDir(dir)
+	for _, f := range files {
+		file := filepath.Join(dir, f.Name())
+		args = []string{
+			"scp",
+			"-r",
+			file,
+			fmt.Sprintf("%s:%s", system.MachineName, hostDir),
+		}
+		cmd = exec.Command(rawCmd, args...)
+		err = cmd.Run()
+		if err != nil {
+			return "", fmt.Errorf("Could not upload file %s\n%s", f.Name(), err)
+		}
+	}
+
+	return hostDir, nil
 }
