0ceade0e31118bd0a13da687bde31c3956970743
Justin Carter <jcarter@starkandwayne.com>
2016-06-06 14:49:33 +0200

    Add Makefile and build script

---
+++ Makefile
@@ -0,0 +1,22 @@
+EXTERNAL_TOOLS=\
+	       github.com/kardianos/govendor \
+	       github.com/mitchellh/gox \
+
+all: test
+
+dev:
+	@DELMO_DEV=1 sh -c "'$(PWD)/scripts/build.sh'"
+
+test:
+	scripts/test.sh
+
+build:
+	scripts/build.sh
+
+bootstrap:
+	@for tool in  $(EXTERNAL_TOOLS) ; do \
+		echo "Installing $$tool" ; \
+		go get $$tool; \
+	done
+
+PHONY: all test
---
+++ scripts/build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# This script builds the application from source for multiple platforms.
+set -e
+
+VERSION=${VERSION:-(development)}
+BINARY=${BINARY:-delmo}
+
+# Get the parent directory of where this script is.
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ] ; do SOURCE="$(readlink "$SOURCE")"; done
+DIR="$( cd -P "$( dirname "$SOURCE" )/.." && pwd )"
+
+# Change into that directory
+cd "$DIR"
+
+# Delete the old dir
+echo "==> Removing old directory..."
+rm -f bin/*
+rm -rf pkg/*
+mkdir -p bin
+
+TARGETS=${TARGETS:-linux/amd64 darwin/amd64}
+# If its dev mode, only build for ourself
+if [[ ! -z "${DELMO_DEV}" ]]; then
+  TARGETS=$(go env GOOS)/$(go env GOARCH)
+fi
+
+
+# Build!
+echo "==> Building..."
+gox -osarch="${TARGETS}" --output="pkg/${BINARY}-{{.OS}}-{{.Arch}}" -ldflags="-X main.Version=${VERSION}" ./...
+
+DEV_BINARY="${BINARY}-$(go env GOOS)-$(go env GOARCH)"
+cp pkg/${DEV_BINARY} bin/
+cp pkg/${DEV_BINARY} ${GOPATH}/bin/
+
+echo "Built version: $(bin/${DEV_BINARY} --version)"
