d45ed70abc2aeff893723876fbcff3a4ac1a2e39
Justin Carter <jcarter@starkandwayne.com>
2016-06-06 16:12:36 +0200

    Fix export

--- ci/tasks/shipit.sh
+++ ci/tasks/shipit.sh
@@ -23,7 +23,7 @@ fi
 
 ###############################################################
 export GOPATH=$PWD/delmo:$GOPATH
-export PATH:$GOPATH/bin:$PATH
+export PATH=$GOPATH/bin:$PATH
 
 pushd ${REPO_ROOT}
 go get github.com/mitchellh/gox
