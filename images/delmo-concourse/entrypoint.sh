6d491cab2889b447bd941dc8c4333e17bbf12d07
Justin Carter <jcarter@starkandwayne.com>
2016-06-07 20:13:55 +0200

    Better output in entrypoint.sh

--- images/delmo-concourse/entrypoint.sh
+++ images/delmo-concourse/entrypoint.sh
@@ -3,14 +3,16 @@
 if [ ! -z ${MACHINE_NAME} ] && [ ! -z ${AWS_ACCESS_KEY_ID} ] \
     && [ ! -z ${AWS_SECRET_ACCESS_KEY} ] &&  [ ! -z ${AWS_DEFAULT_REGION} ] \
     && [ ! -z ${AWS_BUCKET} ]; then
-    echo "Downloading pre existing configuration"
+    echo "Downloading pre existing docker-machine configuration..."
     aws --region ${AWS_DEFAULT_REGION} s3 cp s3://${AWS_BUCKET}/${MACHINE_NAME}.zip ./ || exit 1
 
-    echo "Pre-existing configuration found"
-    echo "Importing ${MACHINE_NAME}"
+    echo "Importing configuration..."
     machine-import ${MACHINE_NAME}.zip
     # The permission isn't set properly on import
     chmod 0600 /root/.docker/machine/machines/${MACHINE_NAME}/id_rsa
+
+    echo "Machine ${MACHINE_NAME} imported!"
 fi
 
+echo "Running '$@'"
 exec $@
