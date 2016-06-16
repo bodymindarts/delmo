85fc166375fc2212f293b231de02e668a76db526
Justin Carter <jcarter@starkandwayne.com>
2016-06-16 15:52:30 +0200

    Accept MACHINE_EXPORT prefix to env vars on delmo-concourse

--- images/delmo-concourse/entrypoint.sh
+++ images/delmo-concourse/entrypoint.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+set -x
 
-if [ ! -z ${MACHINE_NAME} ] && [ ! -z ${AWS_ACCESS_KEY_ID} ] \
-    && [ ! -z ${AWS_SECRET_ACCESS_KEY} ] &&  [ ! -z ${AWS_DEFAULT_REGION} ] \
-    && [ ! -z ${AWS_BUCKET} ]; then
+if [ ! -z ${MACHINE_NAME} ] && [ ! -z ${MACHINE_EXPORT_AWS_ACCESS_KEY_ID} ] \
+    && [ ! -z ${MACHINE_EXPORT_AWS_SECRET_ACCESS_KEY} ] &&  [ ! -z ${MACHINE_EXPORT_AWS_REGION} ] \
+    && [ ! -z ${MACHINE_EXPORT_AWS_BUCKET} ]; then
     echo "Downloading pre existing docker-machine configuration..."
-    aws --region ${AWS_DEFAULT_REGION} s3 cp s3://${AWS_BUCKET}/${MACHINE_NAME}.zip ./ || exit 1
+    (
+      export AWS_ACCESS_KEY_ID=${MACHINE_EXPORT_AWS_ACCESS_KEY_ID}
+      export AWS_SECRET_ACCESS_KEY=${MACHINE_EXPORT_AWS_SECRET_ACCESS_KEY}
+      export AWS_DEFAULT_REGION=${MACHINE_EXPORT_AWS_REGION}
+      aws --region ${MACHINE_EXPORT_AWS_REGION} s3 cp s3://${MACHINE_EXPORT_AWS_BUCKET}/${MACHINE_NAME}.zip ./
+    ) || exit 1
 
     echo "Importing configuration..."
     machine-import ${MACHINE_NAME}.zip
