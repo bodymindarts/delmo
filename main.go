b1549aff7dfc8cbfba346026a3263362a627836c
Justin Carter <jcarter@starkandwayne.com>
2016-06-20 13:48:17 +0200

    Typo

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func Run(args []string) int {
 	var onlyBuildTask bool
 	flags.StringVar(&delmoFile, "f", "delmo.yml", "Path to the delmo.yml file.")
 	flags.StringVar(&machine, "m", "default", "The docker-machine to use.")
-	flags.BoolVar(&onlyBuildTask, "only-build-task", false, "Only build the task_image. All othe images must be available to via pull.")
+	flags.BoolVar(&onlyBuildTask, "only-build-task", false, "Only build the task_image. All other images must be available via docker pull.")
 	if err := flags.Parse(args); err != nil {
 		printfStdErr("Error parsing arguments\n%s", err)
 		return 2
