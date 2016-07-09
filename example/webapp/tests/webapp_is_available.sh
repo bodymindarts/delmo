041eb784b30885873a393bb0783911ab91ffdcf9
Justin Carter <jcarter@starkandwayne.com>
2016-07-09 14:44:31 +0200

    Add nginx as proxy to webapp example

--- example/webapp/tests/webapp_is_available.sh
+++ example/webapp/tests/webapp_is_available.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-echo "Running test: ${DELMO_TEST_NAME}, host: ${DOCKER_HOST_IP}"
-curl -s ${DOCKER_HOST_IP}:5000
+echo "Running test: ${DELMO_TEST_NAME}"
+curl -si proxy
+curl -si proxy | grep "HTTP/1.1 200 OK" >/dev/null
