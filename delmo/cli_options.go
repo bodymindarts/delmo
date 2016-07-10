5b4a909f83b14dfa91b2aac2b33116a783d77bca
Justin Carter <jcarter@starkandwayne.com>
2016-07-10 11:01:14 +0200

    Help message wording

--- delmo/cli_options.go
+++ delmo/cli_options.go
@@ -27,11 +27,11 @@ OPTIONS:
   -f                    path to the spec file (default: "delmo.yml").
   -m <machine-name>     docker-machine to run the tests on. DOCKER_HOST_IP will
                         be set to the ip returned by 'docker-machine ip <machine>'.
-  --only-build-task     only build the task_image. All other images must be
-                        available via docker pull.
-  --skip-pull           don't pull the images before building.
   --localhost <ip>      an IP that DOCKER_HOST_IP will be set to when not using -m.
   --parallel            execute tests in parallel.
+  --only-build-task     only build the task_image. All other images must be
+                        available via 'docker pull'.
+  --skip-pull           don't pull the images before building.
 `)
 	}
 	flags.Usage = usage
