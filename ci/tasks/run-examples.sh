8e27bedd881de82c87e093c07de503db72d0a97d
Justin Carter <jcarter@starkandwayne.com>
2016-07-10 13:06:34 +0200

    Add empty lines in run-examples.sh

--- ci/tasks/run-examples.sh
+++ ci/tasks/run-examples.sh
@@ -9,16 +9,20 @@ cd delmo/src/github.com/bodymindarts/delmo
 make dev
 BINARY="bin/delmo"
 
+echo
 echo "Downloading machine info"
 aws --region ${AWS_REGION} s3 cp s3://${AWS_BUCKET}/${machine_name}.zip ./
 
+echo
 echo "Importing ${machine_name}"
 machine-import ${machine_name}.zip
 # The permission isn't set properly on import
 chmod 0600 /root/.docker/machine/machines/${machine_name}/id_rsa
 
+echo
 echo "Testing example/webapp"
 ${BINARY} -f example/webapp/delmo.yml -m ${machine_name}
 
+echo
 echo "Executing example/webapp in parralel"
 ${BINARY} -f example/webapp/delmo.yml -m ${machine_name} --parallel
