42c9b17833eb49d6dcdac25dd561ef242590faab
Justin Carter <jcarter@starkandwayne.com>
2016-07-31 17:08:57 +0200

    Use haproxy instead of nginx for proxying

--- example/webapp/sinatra/server.rb
+++ example/webapp/sinatra/server.rb
@@ -1,10 +1,9 @@
 require 'sinatra'
- 
-#Bound to this address so that external hosts can access it, VERY IMPORTANT!
+
 set :bind, '0.0.0.0'
- 
+
 set :logging, true
- 
+
 get '/' do
   'hello world from docker!'
 end
